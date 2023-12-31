3ef6eaa17a97fc8a84398af49f78086b3ea3475d
Jacob Hein <jacobhein1892@gmail.com>
2023-12-30 17:18:51 -0800

    starting work on binary search function

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -8,9 +8,13 @@ using namespace std;
 // set up a function to randomly generate a sorted array of n length
 void generateArray(int* arr)
 {
+    // Generate radom start number
     srand((unsigned) time(NULL));
     int random = rand() % 100;
-    for(int i = 0; i < 5; ++i){
+
+    // Input numbers into array
+    for(int i = 0; i < 12; ++i)
+    {
         *arr = random;
         ++arr;
         ++random;
@@ -18,15 +22,20 @@ void generateArray(int* arr)
 }
 
 // Set up function takes sorted array, and finds number x
+int searchArray(int* arr)
+{
 
+}
 
 int main()
 {
     std::cout << "Lets start a binary Search Algo\n";
-    int array[5];
+    // Initialize array
+    int array[12];
+    
     generateArray(array);
     cout << "After" << endl;
-    for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < 12; ++i){
         cout << array[i] << endl;
     }
     return 0;
